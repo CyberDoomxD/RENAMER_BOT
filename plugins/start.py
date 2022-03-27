73d2ddf3ed44a6df6b81508854d9f417ace2abc5
Cyberguy <91152536+CyberDoomxD@users.noreply.github.com>
2022-03-27 13:06:21 +0530

    start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,10 +12,10 @@ async def start(client,message):
 	**Document Or Video** and enter new filenameto rename it__
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup([[
-          InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/Mo_Tech_Group"), 
-	  InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/MoTech_YT")
+          InlineKeyboardButton("Support Channel" ,url="https://t.me/Djcat_Bot"), 
+	  InlineKeyboardButton("Owner", url="https://t.me/dobby_free)
           ],[
-          InlineKeyboardButton("🧩 Deploy 🧩", url="https://youtu.be/7ALYUk-rYMc")
+          InlineKeyboardButton("Repo 🌚", url="https://t.me/+qPnXZrXRDds2MjEx")
           ]]
           )
         )
